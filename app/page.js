47c987941eacc215b197b9ae7cb08b1d02644c9e
addymwenda12 <muneneaddy@gmail.com>
2024-07-31 18:03:24 +0300

    Files to be ignored

--- app/page.js
+++ app/page.js
@@ -5,9 +5,39 @@ import {firestore} from "@/firebase";
 import { Box, Typography } from "@mui/material";
 
 export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState('');
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'));
+    const docs = await getDocs(snapshot);
+    const inventoryList = []
+    docs.forEach((doc) => {
+      inventoryList.push({
+        id: doc.id,
+        ...doc.data(),
+      })
+    })
+    setInventory(inventoryList);
+  }
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
   return (
     <Box>
-      <Typography variant h1>Inventory app</Typography>
+      <Typography variant='h1'>Inventory app</Typography>
+      {
+        inventory.forEach((item) => {
+          return (
+            <Box key={item.id}>
+              <Typography>{item.name}</Typography>
+              <Typography>{item.quantity}</Typography>
+            </Box>
+          )
+        })
+      }
     </Box>
   );
 }
